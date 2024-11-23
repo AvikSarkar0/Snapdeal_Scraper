c8935b0f577dd8bb2c503726130fd98ccf66b818
AvikSarkar0 <aviksarkar44545@gmail.com>
2024-11-23 18:21:00 +0530

    Update main.py

--- fast_api.py
+++ fast_api.py
@@ -1,19 +1,29 @@
+from fastapi import FastAPI
 from Snapdeal_module.url_scraper import get_url
 from Snapdeal_module.detail_scraper import get_product_name, get_product_price, get_product_discounted_price, \
     get_product_star, get_seller_name, get_seller_star
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 import pandas as pd
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
+import json
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 @app.get("/scrape")
 async def scrape(user_input: str):
-    url = 'https://www.snapdeal.com/'
-    driver = webdriver.Chrome()
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
 
+    chromedriver_path = "/usr/bin/chromedriver"
+    service = Service(chromedriver_path)
+
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+
+    url = 'https://www.snapdeal.com/'
     name1 = []
     price1 = []
     discount1 = []
@@ -25,7 +35,6 @@ async def scrape(user_input: str):
     product_links = get_url(enter_the_product=user_input, driver=driver, url=url)
 
     for link in product_links:
-
         name = get_product_name(product_link=link, driver=driver)
         name1.append(name)
 
@@ -46,9 +55,20 @@ async def scrape(user_input: str):
 
         link1.append(link)
 
+    dataframe = pd.DataFrame({
+        "name": name1,
+        "price": price1,
+        "discount": discount1,
+        "star": star1,
+        "seller_name": seller_name1,
+        "seller_star": seller_star1,
+        "link": link1
+    })
 
-        dataframe = pd.DataFrame({"name":name1, "price":price1, "discount":discount1, "star":star1, "seller_name":seller_name1, "seller_star":seller_star1, "link":link1})
+    dataframe.to_csv("data.csv", index=False)
 
-        dataframe.to_csv("data.csv")
+    json_data = dataframe.to_json(orient='records')
+    beautified_json = json.dumps(json.loads(json_data), indent=4)
+    driver.quit()
 
-    return "scrape complete"
+    return beautified_json
